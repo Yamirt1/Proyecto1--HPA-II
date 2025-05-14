78ce27a996f95a940e54b35805a476a637e8dbbe
Yamirt <yamirpoke@gmail.com>
2025-05-14 00:31:08 -0500

    Terminado con metodos!

--- src/problema2ME.java
+++ src/problema2ME.java
@@ -1,4 +1,3 @@
-
 import javax.swing.JOptionPane;
 
 public class problema2ME {
@@ -6,7 +5,7 @@ public class problema2ME {
     static int mulinoPA = 0, mulinoPO = 0, mulinoCH = 0, mulinoCO = 0;
     static int lombanaPA = 0, lombanaPO = 0, lombanaCH = 0, lombanaCO = 0;
     static int torrijosPA = 0, torrijosPO = 0, torrijosCH = 0, torrijosCO = 0;
-    static int votosBlancoPA = 0, votosBlancoPO = 0, votosBlancoCH = 0, votosBlancoCO = 0;
+    static int votBlancoPA = 0, votBlancoPO = 0, votBlancoCH = 0, votBlancoCO = 0;
 
     public static void menuPrincipal() { //1 - Metodo!
         boolean salir = false;
@@ -267,17 +266,301 @@ public class problema2ME {
         }
     }
 
-    public static void mostrarVotosPorProvincia(){ //5 Metodo!
+    public static void mostrarVotosPorProvincia() { //5 Metodo!
 
+        String conteo = "VOTOS POR PROVINCIA EN ORDEN DE MAYOR A MENOR";
+
+        conteo += "\nProvincia de Panamá\n";
+        if (mulinoPA >= lombanaPA && mulinoPA >= torrijosPA) {
+            conteo += "1- José Raúl Mulino: " + mulinoPA + " votos\n";
+            if (lombanaPA >= torrijosPA) {
+                conteo += "2- Ricardo Lombana: " + lombanaPA + " votos\n";
+                conteo += "3- Martin Torrijos: " + torrijosPA + " votos\n";
+            } else {
+                conteo += "2- Martin Torrijos: " + torrijosPA + " votos\n";
+                conteo += "3- Ricardo Lombana: " + lombanaPA + " votos\n";
+            }
+        } else if (lombanaPA >= mulinoPA && lombanaPA >= torrijosPA) {
+            conteo += "1- Ricardo Lombana: " + lombanaPA + " votos\n";
+            if (mulinoPA >= torrijosPA) {
+                conteo += "2- José Raúl Mulino: " + mulinoPA + " votos\n";
+                conteo += "3- Martin Torrijos: " + torrijosPA + " votos\n";
+            } else {
+                conteo += "2- Martin Torrijos: " + torrijosPA + " votos\n";
+                conteo += "3- José Raúl Mulino: " + mulinoPA + " votos\n";
+            }
+        } else {
+            conteo += "1- Martin Torrijos: " + torrijosPA + " votos\n";
+            if (mulinoPA >= lombanaPA) {
+                conteo += "2- José Raúl Mulino: " + mulinoPA + " votos\n";
+                conteo += "3- Ricardo Lombana: " + lombanaPA + " votos\n";
+            } else {
+                conteo += "2- Ricardo Lombana: " + lombanaPA + " votos\n";
+                conteo += "3- José Raúl Mulino: " + mulinoPA + " votos\n";
+            }
+        }
+
+        conteo += "\nProvincia de Panamá Oeste\n";
+        if (mulinoPO >= lombanaPO && mulinoPO >= torrijosPO) {
+            conteo += "1- José Raúl Mulino: " + mulinoPO + " votos\n";
+            if (lombanaPO >= torrijosPO) {
+                conteo += "2- Ricardo Lombana: " + lombanaPO + " votos\n";
+                conteo += "3- Martin Torrijos: " + torrijosPO + " votos\n";
+            } else {
+                conteo += "2- Martin Torrijos: " + torrijosPO + " votos\n";
+                conteo += "3- Ricardo Lombana: " + lombanaPO + " votos\n";
+            }
+        } else if (lombanaPO >= mulinoPO && lombanaPO >= torrijosPO) {
+            conteo += "1- Ricardo Lombana: " + lombanaPO + " votos\n";
+            if (mulinoPO >= torrijosPO) {
+                conteo += "2- José Raúl Mulino: " + mulinoPO + " votos\n";
+                conteo += "3- Martin Torrijos: " + torrijosPO + " votos\n";
+            } else {
+                conteo += "2- Martin Torrijos: " + torrijosPO + " votos\n";
+                conteo += "3- José Raúl Mulino: " + mulinoPO + " votos\n";
+            }
+        } else {
+            conteo += "1- Martin Torrijos: " + torrijosPO + " votos\n";
+            if (mulinoPO >= lombanaPO) {
+                conteo += "2- José Raúl Mulino: " + mulinoPO + " votos\n";
+                conteo += "3- Ricardo Lombana: " + lombanaPO + " votos\n";
+            } else {
+                conteo += "2- Ricardo Lombana: " + lombanaPO + " votos\n";
+                conteo += "3- José Raúl Mulino: " + mulinoPO + " votos\n";
+            }
+        }
+
+        conteo += "\nProvincia de Chiriquí\n";
+        if (mulinoCH >= lombanaCH && mulinoCH >= torrijosCH) {
+            conteo += "1- José Raúl Mulino: " + mulinoCH + " votos\n";
+            if (lombanaCH >= torrijosCH) {
+                conteo += "2- Ricardo Lombana: " + lombanaCH + " votos\n";
+                conteo += "3- Martin Torrijos: " + torrijosCH + " votos\n";
+            } else {
+                conteo += "2- Martin Torrijos: " + torrijosCH + " votos\n";
+                conteo += "3- Ricardo Lombana: " + lombanaCH + " votos\n";
+            }
+        } else if (lombanaCH >= mulinoCH && lombanaCH >= torrijosCH) {
+            conteo += "1- Ricardo Lombana: " + lombanaCH + " votos\n";
+            if (mulinoCH >= torrijosCH) {
+                conteo += "2- José Raúl Mulino: " + mulinoCH + " votos\n";
+                conteo += "3- Martin Torrijos: " + torrijosCH + " votos\n";
+            } else {
+                conteo += "2- Martin Torrijos: " + torrijosCH + " votos\n";
+                conteo += "3- José Raúl Mulino: " + mulinoCH + " votos\n";
+            }
+        } else {
+            conteo += "1- Martin Torrijos: " + torrijosCH + " votos\n";
+            if (mulinoCH >= lombanaCH) {
+                conteo += "2- José Raúl Mulino: " + mulinoCH + " votos\n";
+                conteo += "3- Ricardo Lombana: " + lombanaCH + " votos\n";
+            } else {
+                conteo += "2- Ricardo Lombana: " + lombanaCH + " votos\n";
+                conteo += "3- José Raúl Mulino: " + mulinoCH + " votos\n";
+            }
+        }
+
+        conteo += "\nProvincia de Colón\n";
+        if (mulinoCO >= lombanaCO && mulinoCO >= torrijosCO) {
+            conteo += "1- José Raúl Mulino: " + mulinoCO + " votos\n";
+            if (lombanaCO >= torrijosCO) {
+                conteo += "2- Ricardo Lombana: " + lombanaCO + " votos\n";
+                conteo += "3- Martin Torrijos: " + torrijosCO + " votos\n";
+            } else {
+                conteo += "2- Martin Torrijos: " + torrijosCO + " votos\n";
+                conteo += "3- Ricardo Lombana: " + lombanaCO + " votos\n";
+            }
+        } else if (lombanaCO >= mulinoCO && lombanaCO >= torrijosCO) {
+            conteo += "1- Ricardo Lombana: " + lombanaCO + " votos\n";
+            if (mulinoCO >= torrijosCO) {
+                conteo += "2- José Raúl Mulino: " + mulinoCO + " votos\n";
+                conteo += "3- Martin Torrijos: " + torrijosCO + " votos\n";
+            } else {
+                conteo += "2- Martin Torrijos: " + torrijosCO + " votos\n";
+                conteo += "3- José Raúl Mulino: " + mulinoCO + " votos\n";
+            }
+        } else {
+            conteo += "1- Martin Torrijos: " + torrijosCO + " votos\n";
+            if (mulinoCO >= lombanaCO) {
+                conteo += "2- José Raúl Mulino: " + mulinoCO + " votos\n";
+                conteo += "3- Ricardo Lombana: " + lombanaCO + " votos\n";
+            } else {
+                conteo += "2- Ricardo Lombana: " + lombanaCO + " votos\n";
+                conteo += "3- José Raúl Mulino: " + mulinoCO + " votos\n";
+            }
+        }
+        JOptionPane.showMessageDialog(null, conteo);
     }
 
-    public static void mostrarVotosTotales(){ //6 metodo!
+    public static void mostrarVotosTotales() { //6 - Metodo!
 
+        int mulinoTotales = mulinoPA + mulinoPO + mulinoCH + mulinoCO;
+        int lombanaTotales = lombanaPA + lombanaPO + lombanaCH + lombanaCO;
+        int torrijosTotales = torrijosPA + torrijosPO + torrijosCH + torrijosCO;
+
+        String resultado = "VOTOS TOTALES POR CANDIDATO EN ORDEN DE MAYOR A MENOR\n";
+
+        if (mulinoTotales >= lombanaTotales && mulinoTotales >= torrijosTotales) {
+            resultado += "1. José Raúl Mulino: " + mulinoTotales + " votos\n";
+            if (lombanaTotales >= torrijosTotales) {
+                resultado += "2. Ricardo Lombana: " + lombanaTotales + " votos\n";
+                resultado += "3. Martin Torrijos: " + torrijosTotales + " votos\n";
+            } else {
+                resultado += "2. Martin Torrijos: " + torrijosTotales + " votos\n";
+                resultado += "3. Ricardo Lombana: " + lombanaTotales + " votos\n";
+            }
+        } else if (lombanaTotales >= mulinoTotales && lombanaTotales >= torrijosTotales) {
+            resultado += "1. Ricardo Lombana: " + lombanaTotales + " votos\n";
+            if (mulinoTotales >= torrijosTotales) {
+                resultado += "2. José Raúl Mulino: " + mulinoTotales + " votos\n";
+                resultado += "3. Martin Torrijos: " + torrijosTotales + " votos\n";
+            } else {
+                resultado += "2. Martin Torrijos: " + torrijosTotales + " votos\n";
+                resultado += "3. José Raúl Mulino: " + mulinoTotales + " votos\n";
+            }
+        } else {
+            resultado += "1. Martin Torrijos: " + torrijosTotales + " votos\n";
+            if (mulinoTotales >= lombanaTotales) {
+                resultado += "2. José Raúl Mulino: " + mulinoTotales + " votos\n";
+                resultado += "3. Ricardo Lombana: " + lombanaTotales + " votos\n";
+            } else {
+                resultado += "2. Ricardo Lombana: " + lombanaTotales + " votos\n";
+                resultado += "3. José Raúl Mulino: " + mulinoTotales + " votos\n";
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, resultado);
     }
 
-    public static void mostrarVotosBlancoOrdenados(){ // 7 metodo!
+    public static void mostrarVotosBlancoOrdenados() { // 7 metodo!
 
+        String conteoBlancos = "VOTOS EN BLANCO POR PROVINCIA (MAYOR A MENOR):\n";
+
+        if (votBlancoPA >= votBlancoPO && votBlancoPA >= votBlancoCH && votBlancoPA >= votBlancoCO) {
+            conteoBlancos += "1. Provincia de Panamá: " + votBlancoPA + " votos\n";
+            if (votBlancoPO >= votBlancoCH && votBlancoPO >= votBlancoCO) {
+                conteoBlancos += "2. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                if (votBlancoCH >= votBlancoCO) {
+                    conteoBlancos += "3. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                    conteoBlancos += "4. Provincia de Colón: " + votBlancoCO + " votos\n";
+                } else {
+                    conteoBlancos += "3. Provincia de Colón: " + votBlancoCO + " votos\n";
+                    conteoBlancos += "4. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                }
+            } else if (votBlancoCH >= votBlancoPO && votBlancoCH >= votBlancoCO) {
+                conteoBlancos += "2. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                if (votBlancoPO >= votBlancoCO) {
+                    conteoBlancos += "3. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                    conteoBlancos += "4. Provincia de Colón: " + votBlancoCO + " votos\n";
+                } else {
+                    conteoBlancos += "3. Provincia de Colón: " + votBlancoCO + " votos\n";
+                    conteoBlancos += "4. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                }
+            } else {
+                conteoBlancos += "2. Provincia de Colón: " + votBlancoCO + " votos\n";
+                if (votBlancoPO >= votBlancoCH) {
+                    conteoBlancos += "3. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                    conteoBlancos += "4. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                } else {
+                    conteoBlancos += "3. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                    conteoBlancos += "4. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                }
+            }
+        } else if (votBlancoPO >= votBlancoPA && votBlancoPO >= votBlancoCH && votBlancoPO >= votBlancoCO) {
+            conteoBlancos += "1. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+            if (votBlancoPA >= votBlancoCH && votBlancoPA >= votBlancoCO) {
+                conteoBlancos += "2. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                if (votBlancoCH >= votBlancoCO) {
+                    conteoBlancos += "3. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                    conteoBlancos += "4. Provincia de Colón: " + votBlancoCO + " votos\n";
+                } else {
+                    conteoBlancos += "3. Provincia de Colón: " + votBlancoCO + " votos\n";
+                    conteoBlancos += "4. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                }
+            } else if (votBlancoCH >= votBlancoPA && votBlancoCH >= votBlancoCO) {
+                conteoBlancos += "2. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                if (votBlancoPA >= votBlancoCO) {
+                    conteoBlancos += "3. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                    conteoBlancos += "4. Provincia de Colón: " + votBlancoCO + " votos\n";
+                } else {
+                    conteoBlancos += "3. Provincia de Colón: " + votBlancoCO + " votos\n";
+                    conteoBlancos += "4. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                }
+            } else {
+                conteoBlancos += "2. Provincia de Colón: " + votBlancoCO + " votos\n";
+                if (votBlancoPA >= votBlancoCH) {
+                    conteoBlancos += "3. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                    conteoBlancos += "4. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                } else {
+                    conteoBlancos += "3. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                    conteoBlancos += "4. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                }
+            }
+        } else if (votBlancoCH >= votBlancoPA && votBlancoCH >= votBlancoPO && votBlancoCH >= votBlancoCO) {
+            conteoBlancos += "1. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+            if (votBlancoPA >= votBlancoPO && votBlancoPA >= votBlancoCO) {
+                conteoBlancos += "2. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                if (votBlancoPO >= votBlancoCO) {
+                    conteoBlancos += "3. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                    conteoBlancos += "4. Provincia de Colón: " + votBlancoCO + " votos\n";
+                } else {
+                    conteoBlancos += "3. Provincia de Colón: " + votBlancoCO + " votos\n";
+                    conteoBlancos += "4. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                }
+            } else if (votBlancoPO >= votBlancoPA && votBlancoPO >= votBlancoCO) {
+                conteoBlancos += "2. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                if (votBlancoPA >= votBlancoCO) {
+                    conteoBlancos += "3. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                    conteoBlancos += "4. Provincia de Colón: " + votBlancoCO + " votos\n";
+                } else {
+                    conteoBlancos += "3. Provincia de Colón: " + votBlancoCO + " votos\n";
+                    conteoBlancos += "4. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                }
+            } else {
+                conteoBlancos += "2. Provincia de Colón: " + votBlancoCO + " votos\n";
+                if (votBlancoPA >= votBlancoPO) {
+                    conteoBlancos += "3. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                    conteoBlancos += "4. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                } else {
+                    conteoBlancos += "3. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                    conteoBlancos += "4. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                }
+            }
+        } else {
+            conteoBlancos += "1. Provincia de Colón: " + votBlancoCO + " votos\n";
+            if (votBlancoPA >= votBlancoPO && votBlancoPA >= votBlancoCH) {
+                conteoBlancos += "2. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                if (votBlancoPO >= votBlancoCH) {
+                    conteoBlancos += "3. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                    conteoBlancos += "4. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                } else {
+                    conteoBlancos += "3. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                    conteoBlancos += "4. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                }
+            } else if (votBlancoPO >= votBlancoPA && votBlancoPO >= votBlancoCH) {
+                conteoBlancos += "2. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                if (votBlancoPA >= votBlancoCH) {
+                    conteoBlancos += "3. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                    conteoBlancos += "4. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                } else {
+                    conteoBlancos += "3. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                    conteoBlancos += "4. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                }
+            } else {
+                conteoBlancos += "2. Provincia de Chiriquí: " + votBlancoCH + " votos\n";
+                if (votBlancoPA >= votBlancoPO) {
+                    conteoBlancos += "3. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                    conteoBlancos += "4. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                } else {
+                    conteoBlancos += "3. Provincia de Panamá Oeste: " + votBlancoPO + " votos\n";
+                    conteoBlancos += "4. Provincia de Panamá: " + votBlancoPA + " votos\n";
+                }
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, conteoBlancos);
     }
+
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Bienvenido al tribunal electoral");
 
